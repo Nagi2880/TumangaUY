6ec980d07a302d41b3479939cc4882ea47994899
Nagi2880 <alexanderamaral17@gmail.com>
2022-06-16 14:50:44 -0300

    Starting with typescript

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Webpagelogo from '../elements/webpagelogo';
+
+function Header() {
+  return (
+    <header>
+        <Webpagelogo />
+    </header>
+ );
+};
+
+export default Header;
---
+++ src/elements/webpagelogo.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function webpagelogo() {
+  return (
+    <div className="Pagelogo-Container">
+        <img src='../logomanga.jpg' alt='logomanga' />
+        <h1>Manga Comic Uy</h1>
+
+    </div>
+  )
+}
+
+export default webpagelogo
